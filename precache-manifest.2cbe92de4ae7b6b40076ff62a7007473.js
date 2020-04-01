d01b4dfdcd78322cfb79ca7228c6a93037073b62
condee78 <condee78@gmail.com>
2020-04-02 02:03:23 +0700

    Updates

---
+++ precache-manifest.2cbe92de4ae7b6b40076ff62a7007473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b06138d21a1ac1c7c17f1942821817",
+    "url": "/firstreactjs/index.html"
+  },
+  {
+    "revision": "fdb22cb52a7e2c6ff659",
+    "url": "/firstreactjs/static/css/main.39f41730.chunk.css"
+  },
+  {
+    "revision": "2f8567aad5720b6a4684",
+    "url": "/firstreactjs/static/js/2.f45ccda4.chunk.js"
+  },
+  {
+    "revision": "fdb22cb52a7e2c6ff659",
+    "url": "/firstreactjs/static/js/main.269d7cb5.chunk.js"
+  },
+  {
+    "revision": "f1e2385d6a4b37ee98e7",
+    "url": "/firstreactjs/static/js/runtime-main.16682fee.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/firstreactjs/static/media/logo.25bf045c.svg"
+  }
+]);
